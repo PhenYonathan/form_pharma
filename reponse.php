e6268c07f9ecf90c92fc4876f752ee47a364b91a
PhenYonathan <yonathancardoso@outlook.fr>
2022-02-11 16:37:59 +0100

    (progress) page de confirmation

--- reponse.php
+++ reponse.php
@@ -19,18 +19,25 @@
 <head>
     <meta charset="utf-8" />
     <title>Vérifications</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="mail.php">
-        <h1>Bonjour, vous êtes bien <?php echo $_POST['nom'] ?> ?</h1>
-        <h2>Votre email est  <?php echo $_POST['mail'] ?></h2>
-        <h2>Votre ordonnance est bien celle ci ? <br>
-            <embed src="<?php $_POST['ordonnance'] ?>" width=800 height=500 type='application/pdf'/>
-        </h2>
-        <br>
-        <label>Si toutes les informations sont correctes merci de </label>
-        <input type="submit" value="Confirmer" disabled>
-    </form>
-    
+    <div class="login-box">
+        <form class="recap" action="mail.php">
+            <h1>Bonjour, vous êtes bien <?php echo $_POST['nom'] ?> ?</h1>
+            <h2>Votre email est  <?php echo $_POST['mail'] ?></h2>
+            <h3>Votre ordonnance est bien celle ci ?</h3>
+            <img class="img_pres" src="assets/img_temp.jpg" alt="">
+
+            <hr class="marge">
+
+            <div class="confirm">
+                <label>Si toutes les informations sont correctes merci de </label>
+                <input type="submit" value="Confirmer" disabled>
+            </div>
+        </form>
+    </div>
+
 </body>
 </html>
